1471b5d9fbd852dc854aaf00102bf0c7f9a8d515
Bin Guo <18500056398@163.com>
2021-12-20 17:25:59 +0800

    Optimize sampling functions

--- src/warp.cpp
+++ src/warp.cpp
@@ -10,8 +10,6 @@
 
 NORI_NAMESPACE_BEGIN
 
-static double Pi = 3.14159265358979;
-
 Point2f Warp::squareToUniformSquare(const Point2f &sample) {
     return sample;
 }
@@ -33,37 +31,37 @@ float Warp::squareToTentPdf(const Point2f &p) {
 
 Point2f Warp::squareToUniformDisk(const Point2f &sample) {
 	auto r = std::sqrt(sample.x());
-	auto theta = sample.y() * 2 * Pi;
+	auto theta = sample.y() * 2 * M_PI;
 	return Point2f(r * std::cosf(theta), r * std::sinf(theta));
 }
 
 float Warp::squareToUniformDiskPdf(const Point2f &p) {
-    return p.x() * p.x() + p.y() * p.y() <= 1.f ? 1.f / Pi : 0;
+    return p.x() * p.x() + p.y() * p.y() <= 1.f ? M_1_PI : 0;
 }
 
 Vector3f Warp::squareToUniformSphere(const Point2f &sample) {
     auto theta = std::acosf(1.f-2*sample.x());
-    auto phi = 2 * Pi * sample.y();
+    auto phi = 2 * M_PI * sample.y();
     return Vector3f(std::sinf(theta)*std::cosf(phi),std::cosf(theta),std::sinf(theta)*std::sinf(phi));
 }
 
 float Warp::squareToUniformSpherePdf(const Vector3f &v) {
-	return 1.f / (4 * Pi);
+	return 1.f / (4 * M_PI);
 }
 
 Vector3f Warp::squareToUniformHemisphere(const Point2f &sample) {
 	auto theta = std::acosf(1.f-2*sample.x());
-	auto phi = Pi * sample.y();
+	auto phi = M_PI * sample.y();
 	return Vector3f(std::sinf(theta)*std::cosf(phi),std::cosf(theta),std::sinf(theta)*std::sinf(phi));
 }
 
 float Warp::squareToUniformHemispherePdf(const Vector3f &v) {
-	return v.z() >= 0 ? 1.f / (2 * Pi) : 0;
+	return v.z() >= 0 ? 1.f / (2 * M_PI) : 0;
 }
 
 Vector3f Warp::squareToCosineHemisphere(const Point2f &sample) {
 	auto r = std::sqrt(sample.x());
-	auto theta = sample.y() * 2 * Pi;
+	auto theta = sample.y() * 2 * M_PI;
 	auto x = r * std::cosf(theta);
 	auto y = r * std::sinf(theta);
 	return Vector3f(x, y, std::sqrtf(1.f - x*x - y*y));
@@ -71,15 +69,15 @@ Vector3f Warp::squareToCosineHemisphere(const Point2f &sample) {
 
 float Warp::squareToCosineHemispherePdf(const Vector3f &v) {
     auto cos_theta = Vector3f(0,0,1).dot(v.normalized());
-    return cos_theta >= 0.f ? cos_theta/Pi : 0;
+    return cos_theta >= 0.f ? cos_theta*M_1_PI : 0;
 }
 
 Vector3f Warp::squareToBeckmann(const Point2f &sample, float alpha) {
-	auto ln = logf(sample.x());
+	auto ln = logf(1.f - sample.x());
 	auto tan_theta2 = -alpha*alpha*ln;
 	auto cos_theta2 = 1/(1+tan_theta2);
     auto sin_theta = std::sqrtf(std::max(0.f,1-cos_theta2));
-    auto phi = 2 * Pi * sample.y();
+    auto phi = 2 * M_PI * sample.y();
     return Vector3f(sin_theta*std::cosf(phi), sin_theta*std::sinf(phi), std::sqrtf(cos_theta2));
 }
 
@@ -89,7 +87,8 @@ float Warp::squareToBeckmannPdf(const Vector3f &m, float alpha) {
     auto cos_theta2 = cos_theta * cos_theta;
     auto tan_theta2 = (1-cos_theta2)/cos_theta2;
     auto alpha2 = alpha * alpha;
-    return 1 / Pi * std::expf(-tan_theta2/alpha2) / (alpha2*cos_theta2*cos_theta);
+    float cos_theta3 = std::max(1e-20f, cos_theta2*cos_theta);
+    return std::expf(-tan_theta2/alpha2) / (M_PI * alpha2*cos_theta3);
 }
 
 NORI_NAMESPACE_END
