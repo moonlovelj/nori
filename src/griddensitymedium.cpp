fb87e9eae7ff646007c98a71c2ea82356734c435
Bin Guo <18500056398@163.com>
2022-04-01 20:29:25 +0800

    Sample grid density medium

--- src/griddensitymedium.cpp
+++ src/griddensitymedium.cpp
@@ -17,7 +17,13 @@ public:
         sigma_s = propList.getColor("sigma_s", Color3f(0.2f));
         g = propList.getFloat("g", 0.1f);
         sigma_t = sigma_s + sigma_a;
+        if (sigma_t.x() != sigma_t.y() || sigma_t.x() != sigma_t.z()) {
+            std::cout << "GridDensityMedium requires a spectrally uniform attenuation \n";
+        }
+
         m_phase = std::make_shared<HenyeyGreenstein>(g);
+        worldToMedium = propList.getTransform("toWorld", Transform()).inverse();
+        invMaxDensity = 1;
     }
 
     Color3f tr(const Ray3f &ray, Sampler *sampler) const override {
@@ -25,23 +31,23 @@ public:
     }
 
     Color3f sample(const Ray3f &ray, Sampler *sampler, Intersection &its) const override {
-//        int channel = std::min((int) (sampler->next1D() * 3), 2);
-//        float dist = -std::log(1 - sampler->next1D()) / sigma_t[channel];
-//        float t = std::min(dist * ray.d.norm(), ray.maxt);
-//        its.t = t;
-//        bool sampledMedium = t < ray.maxt;
-//        if (sampledMedium) {
-//            its.mediumInterface = this;
-//            its.insideMedium = true;
-//        }
-//
-//        Color3f e = -sigma_t * t;
-//        Color3f transmission(std::exp(e.x()), std::exp(e.y()), std::exp(e.z()));
-//
-//        Color3f density = sampledMedium ? (sigma_t * transmission) : transmission;
-//        float pdf = density.x() + density.y() + density.z();
-//        pdf /= 3;
-//        return (Color3f) (transmission / pdf);
+        Ray3f localRay = worldToMedium * ray;
+        const BoundingBox3f b(Point3f(0, 0, 0), Point3f(1, 1, 1));
+        float tMin, tMax;
+        if (!b.rayIntersect(localRay, tMin, tMax)) return Color3f(1);
+
+        float t = tMin;
+        while (true) {
+            t -= std::log(1.f - sampler->next1D()) * invMaxDensity / sigma_t[0];
+            if (t >= tMax) break;
+            if (density(localRay(t)) * invMaxDensity > sampler->next1D()) {
+                its.mediumInterface = this;
+                its.insideMedium = true;
+                its.t = t;
+                return Color3f(1.f / sigma_t[0]);
+            }
+        }
+
         return Color3f(1);
     }
 
@@ -60,6 +66,26 @@ public:
 private:
     Color3f sigma_a, sigma_s, sigma_t;
     float g;
+    float invMaxDensity;
+    int nx, ny, nz;
+    Transform worldToMedium;
+
+    float density(const Point3f &p) const {
+        // Compute voxel coordinates and offsets for _p_
+//        Point3f pSamples(p.x * nx - .5f, p.y * ny - .5f, p.z * nz - .5f);
+//        Point3i pi = (Point3i)Floor(pSamples);
+//        Vector3f d = pSamples - (Point3f)pi;
+//
+//        // Trilinearly interpolate density values to compute local density
+//        Float d00 = Lerp(d.x, D(pi), D(pi + Vector3i(1, 0, 0)));
+//        Float d10 = Lerp(d.x, D(pi + Vector3i(0, 1, 0)), D(pi + Vector3i(1, 1, 0)));
+//        Float d01 = Lerp(d.x, D(pi + Vector3i(0, 0, 1)), D(pi + Vector3i(1, 0, 1)));
+//        Float d11 = Lerp(d.x, D(pi + Vector3i(0, 1, 1)), D(pi + Vector3i(1, 1, 1)));
+//        Float d0 = Lerp(d.y, d00, d10);
+//        Float d1 = Lerp(d.y, d01, d11);
+//        return Lerp(d.z, d0, d1);
+          return 1.f;
+    }
 };
 
 NORI_REGISTER_CLASS(GridDensityMedium, "griddensitymedium");
